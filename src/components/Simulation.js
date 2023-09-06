5ff1a28bc03c9b6f5be6b04c47771345ec347340
Arend Peter Castelein <apc1993@gmail.com>
2023-09-05 19:52:11 -0700

    Make game objects more responsive

--- src/components/Simulation.js
+++ src/components/Simulation.js
@@ -1,23 +1,41 @@
 import { useEffect, useRef, useState } from "react";
 
-class GameObject {
-    constructor(x, y, size){
+class Vector{
+    constructor(x, y=undefined){
         this.x = x;
-        this.y = y;
-        this.size = size;
-        console.log(x, y, size);
+        this.y = y ?? x;
     }
 
-    getStyle(){
+    scale(f){
+        return new Vector(this.x * f, this.y * f);
+    }
+
+    add(v){
+        return new Vector(this.x + v.x, this.y + v.y);
+    }
+
+    asTranslate(){
+        return `translate(${this.x}px, ${this.y}px)`;
+    }
+}
+
+class GameObject {
+    constructor(r, angle, size){
+        this.x = r * Math.cos(Math.PI*angle/180);
+        this.y = r * Math.sin(Math.PI*angle/180);
+        this.size = size;
+    }
+
+    getStyle(containerSize){
         return {
-            width: `${this.size}px`,
-            height: `${this.size}px`,
-            transform: `translate(${this.x}px, ${this.y}px)`
+            width: `${this.size}%`,
+            height: `${this.size}%`,
+            transform: new Vector(.5).add(new Vector(-.5 * this.size / 100)).add(new Vector(this.x /100, this.y /100)).scale(containerSize).asTranslate(),
         }
     } 
 
-    asComponent(){
-        return <div className='object' style={this.getStyle()}/>
+    asComponent(containerSize){
+        return <div className='object' style={this.getStyle(containerSize)}/>
     }
 }
 
@@ -25,19 +43,15 @@ const Simulation = () => {
     let objects = useRef([]);
     let animID = useRef(null);
     let [bool, setBool] = useState(false);
+    let simRef = useRef(null);
 
     const gameLoop = (timestamp) => {
-        console.log(timestamp, objects.current);
         if(objects.current.length == 0){
-            for(let i = 0; i < 10; i++){
-                objects.current.push(new GameObject(
-                    Math.random()*500,
-                    Math.random()*500,
-                    10+Math.random()*60,
-                ))
+            for(let i = 0; i < 360; i += 22.5){
+                objects.current.push(new GameObject(30, i, 10));
             }
         }else{
-            objects.current.forEach(o => o.x += 5);
+            // objects.current.forEach(o => o.x += 5);
         }
 
         setBool(bool => !bool);
@@ -50,8 +64,8 @@ const Simulation = () => {
     }, [])
 
     return (
-        <div className='simulation'>
-            {objects.current.map(o => o.asComponent())}
+        <div ref={simRef} className='simulation'>
+            {objects.current.map(o => o.asComponent(simRef.current.clientHeight))}
         </div>
     )
 }

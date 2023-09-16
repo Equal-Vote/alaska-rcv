2c6bab2fdcf6f8cd0e6a95c746aa6066d85094c8
bestw07 <devspeed.vision.it@gmail.com>
2023-09-16 12:16:16 +0200

    fix border issue

--- src/components/Candidate.js
+++ src/components/Candidate.js
@@ -1,17 +1,18 @@
 import GameObject from "./GameObject";
 
-class Candidate extends GameObject{
-    constructor(r, angle, candidateName){
+class Candidate extends GameObject {
+    constructor(r, angle, candidateName) {
         super(r, angle, 15, -1);
         this.candidateName = candidateName;
         this.customClass = candidateName;
     }
-    
+
     getStyle(containerSize) {
         return {
             ...super.getStyle(containerSize),
             backgroundImage: `url(\"images/${this.candidateName}.jpg\")`,
-            borderWidth: `${Math.round(0.006 * containerSize)}px`,
+            // borderWidth: `${Math.round(0.006 * containerSize)}px`,
+            border: `${Math.round(0.006 * containerSize)}px solid #F11E01`,
         };
     }
 }
--- src/components/GameObject.js
+++ src/components/GameObject.js
@@ -1,7 +1,7 @@
 import { Vector } from "./Vector";
 
 export class GameObject {
-    constructor(r, angle, size, phyMass=undefined) {
+    constructor(r, angle, size, phyMass = undefined) {
         // undefined means don't do physics, negative means static
         this.pos = new Vector(r, angle, true);
         this.vel = new Vector(0);
@@ -26,95 +26,95 @@ export class GameObject {
         };
     }
 
-    getClassNames(simState){
-        return `object ${this.constructor.name} ${this.isVisible(simState)? 'objectVisible': 'objectInvisible'} ${this.customClass}`;
+    getClassNames(simState) {
+        return `object ${this.constructor.name} ${this.isVisible(simState) ? 'objectVisible' : 'objectInvisible'} ${this.customClass}`;
     }
 
     asComponent(simState, containerSize) {
         return <div className={this.getClassNames(simState)} style={this.getStyle(containerSize)} />;
     }
 
-    getSimKey(simState){
-        if(this.simKey != undefined) return this.simKey;
+    getSimKey(simState) {
+        if (this.simKey != undefined) return this.simKey;
         let arr = Object.entries(simState).filter(([k, v]) => v == this);
-        if(arr.length == 0){
+        if (arr.length == 0) {
             this.simKey = '';
-        }else{
+        } else {
             this.simKey = arr[0][0];
         }
         return this.simKey;
     }
 
-    update(){
+    update() {
         this.vel = this.vel.scale(.9); // friction
     }
 
-    canCollidWith(other){ return true; }
+    canCollidWith(other) { return true; }
 
-    isVisible(simState){
-        if(simState.visible.includes(this.constructor)) return true;
+    isVisible(simState) {
+        if (simState.visible.includes(this.constructor)) return true;
         let key = this.getSimKey(simState);
-        if(key == '') return false;
+        if (key == '') return false;
         return simState.visible.includes(key);
     }
-    
-    onCollide(other){}
 
-    applyVelocity(){
+    onCollide(other) { }
+
+    applyVelocity() {
         this.prev_pos = this.pos.clone();
         this.pos = this.pos.add(this.vel);
     }
 
-    revertPos(){
+    revertPos() {
         this.pos = this.prev_pos.clone();
     }
 
-    radius(){
+    radius() {
         return this.size.x / 2;
     }
 
-    tryCollision(other){
-        if(!this.canCollidWith(other) || ! other.canCollidWith(this)) return;
+    tryCollision(other) {
+        if (!this.canCollidWith(other) || !other.canCollidWith(this)) return;
 
         let diff = this.pos.subtract(other.pos);
-        
+
         // normalize
         diff = diff.scale(this.size.add(other.size).scale(.5).invert());
 
         //if(this.is_rect || other.is_rect){
-            //if(normDiff.magnitude() > .5) return false;
-            //if(normDiff.x > normDiff.y){
-                //diff = new Vector(diff.x, 0);
-                //thresh = (this.size.x + other.size.x) / 2;
-            //}else{
-                //diff = new Vector(0, diff.y);
-                //thresh = (this.size.y + other.size.y) / 2;
-            //}
+        //if(normDiff.magnitude() > .5) return false;
+        //if(normDiff.x > normDiff.y){
+        //diff = new Vector(diff.x, 0);
+        //thresh = (this.size.x + other.size.x) / 2;
         //}else{
-            //thresh = this.radius() + other.radius();
+        //diff = new Vector(0, diff.y);
+        //thresh = (this.size.y + other.size.y) / 2;
+        //}
+        //}else{
+        //thresh = this.radius() + other.radius();
         //}
 
         let thresh = 1;
 
-        if(diff.magnitude() > thresh) return false;
+        if (diff.magnitude() > thresh) return false;
 
         let overlap = diff.scaleTo(thresh - diff.magnitude())
         overlap = overlap.scale(this.size.add(other.size).scale(.5));
 
         let t = 0;
-        if(this.phyMass > 0 && other.phyMass > 0 ){
+        if (this.phyMass > 0 && other.phyMass > 0) {
             t = this.phyMass / (this.phyMass + other.phyMass);
-        }else if(this.phyMass > 0){
+        } else if (this.phyMass > 0) {
             t = 0;
-        }else if(other.phyMass > 0){
+        } else if (other.phyMass > 0) {
             t = 1;
-        }else if(this.phyMass == other.phyMass){
+        } else if (this.phyMass == other.phyMass) {
             t = .5;
-        }else{
-            t = (this.phyMass > other.phyMass)? 1 : 0;
+        } else {
+            t = (this.phyMass > other.phyMass) ? 1 : 0;
         }
 
-        this.pos = this.pos.add(overlap.scale(1-t));
+        this.pos = this.pos.add(overlap.scale(1 - t));
         other.pos = other.pos.add(overlap.scale(-t));
 
         other.onCollide(this);

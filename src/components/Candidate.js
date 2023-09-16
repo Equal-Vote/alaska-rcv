8b715009a6c82f95beb04ff06f784eb1eb9f1ba9
Arend Peter Castelein <apc1993@gmail.com>
2023-09-16 11:37:43 -0700

    Remove linting changes

--- src/components/Candidate.js
+++ src/components/Candidate.js
@@ -1,12 +1,12 @@
 import GameObject from "./GameObject";
 
-class Candidate extends GameObject {
-    constructor(r, angle, candidateName) {
+class Candidate extends GameObject{
+    constructor(r, angle, candidateName){
         super(r, angle, 15, -1);
         this.candidateName = candidateName;
         this.customClass = candidateName;
     }
-
+    
     getStyle(containerSize) {
         return {
             ...super.getStyle(containerSize),

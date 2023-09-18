89ae2ec48a34dab6109c7c3b4dd4a6303ceb5ccb
Arend Peter Castelein <apc1993@gmail.com>
2023-09-18 14:13:32 -0700

    Global scroll experiment

--- src/components/Explainer.js
+++ src/components/Explainer.js
@@ -4,13 +4,15 @@ import { SimContext } from "../SimContext";
 const Explainer = () => {
     const {simState, updateSimIndex} = useContext(SimContext);
     const explainerRefs = useRef([]);
+    const containerRef = useRef(null);
 
     let explainers = simState.allExplainers.map((explainer, i) => {
         return <div className="explainerItem" ref={el => explainerRefs.current[i] = el} key={`explainer-${i}`}><div className="explainerInner">{explainer}</div></div>
     })
 
-    let onScroll = (event) => {
-        let mid = (event.target.getBoundingClientRect().top + event.target.getBoundingClientRect().bottom) / 2;
+    let refreshExplainers = (event) => {
+        let rect = containerRef.current.getBoundingClientRect();
+        let mid = (rect.top + rect.bottom) / 2;
 
         let focusedElem = explainerRefs.current.reduce((prev, e, i) => {
             if(i == explainerRefs.current.length-1) return prev;
@@ -29,17 +31,30 @@ const Explainer = () => {
         updateSimIndex(explainerRefs.current.indexOf(focusedElem));
     }
 
-    useEffect(() => {
-        explainerRefs.current.forEach((e, i) => {
-            e.classList.remove('explainerFocused');
-            e.classList.remove('explainerUnfocused');
-            e.classList.add((i == 1)? 'explainerFocused' : 'explainerUnfocused');
-        })
-        updateSimIndex(1);
-    }, [])
+    // I couldn't get global scroll to feel good,so I'm disabling it for now
+    //let onGlobalScroll = (event) => {
+    //    if(containerRef.current.matches(':hover')) return;
+
+    //    containerRef.current.scrollTop += Math.sign(event.deltaY);
+
+    //    refreshExplainers();
+    //}
+
+    //useEffect(() => {
+    //    explainerRefs.current.forEach((e, i) => {
+    //        e.classList.remove('explainerFocused');
+    //        e.classList.remove('explainerUnfocused');
+    //        e.classList.add((i == 1)? 'explainerFocused' : 'explainerUnfocused');
+    //    })
+    //    updateSimIndex(1);
+
+    //    document.addEventListener('wheel', onGlobalScroll);
+    //    
+    //    return () => document.removeEventListener('wheel', onGlobalScroll);
+    //}, [])
 
     return (
-        <div className="explainer" onScroll={onScroll}>
+        <div className="explainer" ref={containerRef} onScroll={refreshExplainers}>
             {explainers}
         </div>
     )

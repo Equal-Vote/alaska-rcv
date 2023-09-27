df4ca2ee1d7d6646265ac6a7782e16039607d285
Arend Peter Castelein <apc1993@gmail.com>
2023-09-27 13:27:59 -0700

    Adjust darken layer

--- src/components/DarkenLayer.js
+++ src/components/DarkenLayer.js
@@ -2,7 +2,8 @@ import GameObject from "./GameObject";
 
 class DarkenLayer extends GameObject{
     constructor(){
-        super('DarkenLayer', 0, 0, 105, undefined);
+        super('DarkenLayer', 0, 0, 100, undefined);
+        this.size.y = this.size.x*1.01; // needs to be slightly taller to fully cover begich
     }
 
     isFocused(){ return undefined; }

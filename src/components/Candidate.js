9be6341ff73fa560c0ac211e4c99dea5e6fa7178
Arend Peter Castelein <apc1993@gmail.com>
2023-10-20 23:50:16 -0700

    Tweak image paths

--- src/components/Candidate.js
+++ src/components/Candidate.js
@@ -24,7 +24,7 @@ class Candidate extends GameObject{
             <div className='CandidateInner' style={{
                 width: '100%',
                 height: '100%',
-                backgroundImage: `url(\"images/${this.candidateName.toLowerCase()}.jpg\")`,
+                backgroundImage: `url(\"/alaska-rcv/images/${this.candidateName.toLowerCase()}.jpg\")`,
                 border: `${Math.round(0.006 * containerSize)}px solid var(--${this.candidateName.toLowerCase()})`,
             }}>
                 <h3 style={{
@@ -32,7 +32,7 @@ class Candidate extends GameObject{
                     margin: `${Math.round(0.002 * containerSize)}px`,
                     fontSize: `${Math.round(0.02 * containerSize)}px`,
                     textAlign: 'center',
-                    marginTop: `${Math.round(.13 * containerSize)}px`,
+                    marginTop: `${Math.round(.125 * containerSize)}px`,
                     backgroundColor: `var(--${this.candidateName.toLowerCase()})`,
                 }}>{this.candidateName}</h3>
             </div>

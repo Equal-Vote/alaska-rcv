a173093aa2f384f07bbc8e41477b12cf18d9e39c
Arend Peter <apc1993@gmail.com>
2024-03-20 16:16:44 -0700

    Update favicon and add feature flag support

--- src/App.js
+++ src/App.js
@@ -2,13 +2,36 @@ import { SimContextProvider } from './SimContext';
 import Simulation from './components/Simulation';
 import Explainer from './components/Explainer';
 import Nav from './components/Nav';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 
 function App() {
-    let [navTop, setNavTop] = useState(0);
+    // FEATURE FLAG
+    const [enabled, setEnabled] = useState(false);
 
-    return (
-        <div className="app">
+    useEffect(() => {
+        let flags = [{
+            key: 'enabled',
+            value: false,
+            description: 'Bypasses the blocker and enables the site',
+            title: 'enabled' // This is optional defaulting to name according to the documentation, but that didn't work for me
+        }]
+
+        const setter = (key, value) => {
+            setEnabled(value);
+        }
+
+        if (window.featureFlagsPluginRegister){
+            window.featureFlagsPluginRegister(flags, setter);
+        }else{
+            window.registerMyFeatureFlags = (register) => register(flags, setter);
+        }
+    }, [])
+
+    // APP
+    let [navTop, setNavTop] = useState(0);
+    if(enabled){
+        return <div className="app">
             <Nav navTop={navTop}/>
             <div className="columns">
                 <SimContextProvider>
@@ -16,8 +39,10 @@ function App() {
                     <Explainer setNavTop={setNavTop}/>
                 </SimContextProvider>
             </div>
-        </div>
-    );
+        </div>;
+    }else{
+        return <div style={{padding: '30px'}}> <h1 style={{color: 'white'}}>Soon to be released...</h1> </div>
+    }
 }
 
 export default App;

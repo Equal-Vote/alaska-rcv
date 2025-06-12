62179c18645e5fb1628fd1edfd17e443975d4537
Arend Peter <apc1993@gmail.com>
2025-06-11 17:33:58 -0700

    Remove bold and fix typo

--- src/content/AlaskaGeneral2022.tsx
+++ src/content/AlaskaGeneral2022.tsx
@@ -11,7 +11,7 @@ export default {
         <p>Voting theorists wondered if the results from the previous election would cause voters to be more strategic in the general, but this wasn't the case.</p>
         <p>Instead voters shifted left across the board and Peltola was the true Condorcet Winner this time.</p>
         <p>There are 2 primary explanations for this <ul>
-            <li>The general election had much more voters, and voters in general elections tend to be more left leaning.</li>
+            <li>The general election had many more voters, and voters in general elections tend to be more left leaning.</li>
             <li>Sarah Palin had the most name recognition going into the special election, and this likely created an electability bias in her favor.
                 Going into the general Peltola was the incumbant, so this shifted the electability bias to her. This implies that one unrepresentative
                 outcome can create a domino effect and give the that candidate an edge in future elections.
